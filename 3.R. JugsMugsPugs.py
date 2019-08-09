4f1778abb08762efe237cddf2c6d6b690c1a2e2b
Mithun Santhosh <42834863+MithunSanthosh1234@users.noreply.github.com>
2019-08-09 10:15:35 -0700

    Update 3.R. JugsMugsPugs.py

--- 3.R. JugsMugsPugs.py
+++ 3.R. JugsMugsPugs.py
@@ -1,3 +1,39 @@
+#JugsMugsPugs 
+
+### Write a program that receives a number on the input.
+### If the number is a multiple of 3, it prints "Jugs". 
+### If the number is a multiple of 5, it prints "Mugs".
+### If the number is a multiple of 7, it prints "Pugs".
+
+### If the number is a multiple of both 3 and 5, it prints "JugsMugs".
+### If the number is a multiple of both 3 and 7, it prints "JugsPugs".
+### If the number is a multiple of both 5 and 7, it prints "MugsPugs".
+### If the number is a multiple of both 3, 5 and 7, it prints "JugsMugsPugs".
+### Otherwise, it prints the number.
+
+```
+INPUT 
+15
+
+OUTPUT
+JugsMugs
+
+INPUT 
+21
+
+OUTPUT
+JugsPugs
+
+
+INPUT 
+105
+
+OUTPUT 
+JugsMugsPugs
+```
+# Python Code:
+
+```
 a = int(input())
 if (a%3 == 0) and (a%5 == 0) and (a%7 == 0):
   print("JugsMugsPugs")
@@ -15,3 +51,4 @@ elif (a%7 == 0):
   print("Pugs")
 else:
   print(a)
+```
