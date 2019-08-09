8df393ccb52b33fd7d0152a08ec3f154c18a2f89
Mithun Santhosh <42834863+MithunSanthosh1234@users.noreply.github.com>
2019-08-09 09:28:00 -0700

    Update and rename 1.7 Two timestamps.py to 1.7 Two timestamps.md

--- 1.7 Two timestamps.py
+++
@@ -1,9 +0,0 @@
-hr1 = int(input())
-min1 = int(input())
-sec1 = int(input()) 
-hr2 = int(input()) 
-min2 = int(input()) 
-sec2 = int(input())
-total1 = min1 * 60 + hr1 * 3600 + sec1
-total2 = min2 * 60 + hr2 * 3600 + sec2
-print(total2 - total1)
